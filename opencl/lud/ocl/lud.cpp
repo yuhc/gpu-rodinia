dfb82fec1513bb0c9603ec40738efd2bc002ce61
Hangchen Yu <hyu@cs.utexas.edu>
2017-09-05 11:55:08 -0500

    [ocl/lud] measure the time

--- opencl/lud/ocl/lud.cpp
+++ opencl/lud/ocl/lud.cpp
@@ -25,6 +25,7 @@
 #include "common.h"
 #include <sys/time.h>
 #include <CL/cl.h>
+#include "timing.h"
 
 #include <string.h>
 #include <string>
@@ -37,12 +38,6 @@
 #else
         #define BLOCK_SIZE 16
 #endif
-
- double gettime() {
-  struct timeval t;
-  gettimeofday(&t,NULL);
-  return t.tv_sec+t.tv_usec*1e-6;
-}
 
 static cl_context	    context;
 static cl_command_queue cmd_queue;
@@ -80,7 +75,11 @@ static int initialize(int platform_id_inuse, int device_id_inuse)
 	printf("Create %s context\n", device_type == CL_DEVICE_TYPE_GPU ? "GPU" : "CPU");
 
 	// create command queue for the first device
-	cmd_queue = clCreateCommandQueue( context, device_list[device_id_inuse], 0, NULL );
+#ifdef TIMING
+        cmd_queue = clCreateCommandQueue( context, device_list[device_id_inuse], CL_QUEUE_PROFILING_ENABLE, NULL );
+#else
+        cmd_queue = clCreateCommandQueue( context, device_list[device_id_inuse], 0, NULL );
+#endif
 	if( !cmd_queue ) { printf("ERROR: clCreateCommandQueue() failed\n"); return -1; }
 	return 0;
 }
@@ -118,7 +117,18 @@ static struct option long_options[] = {
 int
 main ( int argc, char *argv[] )
 {
-  printf("WG size of kernel = %d X %d\n", BLOCK_SIZE, BLOCK_SIZE);
+    //Primitives for timing
+#ifdef TIMING
+    struct timeval tv;
+    struct timeval tv_total_start;
+    struct timeval tv_init_end;
+    struct timeval tv_mem_alloc_end;
+    struct timeval tv_close_start, tv_close_end;
+    float init_time = 0, mem_alloc_time = 0, h2d_time = 0, kernel_time = 0,
+          d2h_time = 0, close_time = 0, total_time = 0;
+#endif
+
+    printf("WG size of kernel = %d X %d\n", BLOCK_SIZE, BLOCK_SIZE);
 	size_t matrix_dim = 32; /* default matrix_dim */
 	int opt, option_index=0;
 	func_ret_t ret;
@@ -212,7 +222,11 @@ main ( int argc, char *argv[] )
 	fclose(fp);
 
 	// OpenCL initialization
+#ifdef  TIMING
+    gettimeofday(&tv_total_start, NULL);
+#endif
 	if(initialize(platform_id_inuse, device_id_inuse)) return -1;
+
 	// compile kernel
 	cl_int err = 0;
 	const char * slist[2] = { source, 0 };
@@ -243,18 +257,34 @@ main ( int argc, char *argv[] )
 	internal  = clCreateKernel(prog, kernel_lud_inter, &err);  
 	if(err != CL_SUCCESS) { printf("ERROR: clCreateKernel() 0 => %d\n", err); return -1; }
 	clReleaseProgram(prog);
-  
+
+#ifdef  TIMING
+    gettimeofday(&tv_init_end, NULL);
+    tvsub(&tv_init_end, &tv_total_start, &tv);
+    init_time = tv.tv_sec * 1000.0 + (float) tv.tv_usec / 1000.0;
+#endif
+
 	//size_t local_work[3] = { 1, 1, 1 };
 	//size_t global_work[3] = {1, 1, 1 }; 
-  
+
 	cl_mem d_m;
 	d_m = clCreateBuffer(context, CL_MEM_READ_WRITE, matrix_dim*matrix_dim * sizeof(float), NULL, &err );
 	if(err != CL_SUCCESS) { printf("ERROR: clCreateBuffer d_m (size:%lu) => %d\n", matrix_dim*matrix_dim, err); return -1;} 
+#ifdef  TIMING
+    gettimeofday(&tv_mem_alloc_end, NULL);
+    tvsub(&tv_mem_alloc_end, &tv_init_end, &tv);
+    mem_alloc_time = tv.tv_sec * 1000.0 + (float) tv.tv_usec / 1000.0;
+#endif
 
 	/* beginning of timing point */
 	stopwatch_start(&sw);
-	err = clEnqueueWriteBuffer(cmd_queue, d_m, 1, 0, matrix_dim*matrix_dim*sizeof(float), m, 0, 0, 0);
+	cl_event event;
+	err = clEnqueueWriteBuffer(cmd_queue, d_m, 1, 0, matrix_dim*matrix_dim*sizeof(float), m, 0, 0, &event);
 	if(err != CL_SUCCESS) { printf("ERROR: clEnqueueWriteBuffer d_m (size:%lu) => %d\n", matrix_dim*matrix_dim, err); return -1; }
+#ifdef TIMING
+    h2d_time += probe_event_time(event,cmd_queue);
+#endif
+    clReleaseEvent(event);
 	
 	size_t i=0;
 	for (i=0; i < matrix_dim-BLOCK_SIZE; i += BLOCK_SIZE) {
@@ -267,8 +297,12 @@ main ( int argc, char *argv[] )
 	  size_t global_work1[3]  = {BLOCK_SIZE, 1, 1};
 	  size_t local_work1[3]  = {BLOCK_SIZE, 1, 1};
 	   
-	  err = clEnqueueNDRangeKernel(cmd_queue, diagnal, 2, NULL, global_work1, local_work1, 0, 0, 0);
+	  err = clEnqueueNDRangeKernel(cmd_queue, diagnal, 2, NULL, global_work1, local_work1, 0, 0, &event);
 	  if(err != CL_SUCCESS) { printf("ERROR:  diagnal clEnqueueNDRangeKernel()=>%d failed\n", err); return -1; }	
+#ifdef TIMING
+      kernel_time += probe_event_time(event,cmd_queue);
+#endif
+      clReleaseEvent(event);
 	  
 	  clSetKernelArg(perimeter, 0, sizeof(void *), (void*) &d_m);
 	  clSetKernelArg(perimeter, 1, sizeof(float) * BLOCK_SIZE * BLOCK_SIZE, (void*)NULL );
@@ -281,8 +315,12 @@ main ( int argc, char *argv[] )
 	  size_t local_work2[3]  = {BLOCK_SIZE * 2, 1, 1};
       // Intel OCL does not support 0 size
       if (global_work2[0] > 0) {
-          err = clEnqueueNDRangeKernel(cmd_queue, perimeter, 2, NULL, global_work2, local_work2, 0, 0, 0);
+          err = clEnqueueNDRangeKernel(cmd_queue, perimeter, 2, NULL, global_work2, local_work2, 0, 0, &event);
           if(err != CL_SUCCESS) { printf("ERROR:  perimeter clEnqueueNDRangeKernel()=>%d failed\n", err); return -1; }	
+#ifdef TIMING
+          kernel_time += probe_event_time(event,cmd_queue);
+#endif
+          clReleaseEvent(event);
       }
 	  
 	  clSetKernelArg(internal, 0, sizeof(void *), (void*) &d_m);
@@ -295,8 +333,12 @@ main ( int argc, char *argv[] )
 	  size_t local_work3[3] = {BLOCK_SIZE, BLOCK_SIZE, 1};
       // Intel OCL does not support 0 size
       if (global_work3[0] > 0) {
-          err = clEnqueueNDRangeKernel(cmd_queue, internal, 2, NULL, global_work3, local_work3, 0, 0, 0);
+          err = clEnqueueNDRangeKernel(cmd_queue, internal, 2, NULL, global_work3, local_work3, 0, 0, &event);
           if(err != CL_SUCCESS) { printf("ERROR:  internal clEnqueueNDRangeKernel()=>%d failed\n", err); return -1; }	
+#ifdef TIMING
+          kernel_time += probe_event_time(event,cmd_queue);
+#endif
+          clReleaseEvent(event);
       }
 	}
 	clSetKernelArg(diagnal, 0, sizeof(void *), (void*) &d_m);
@@ -306,18 +348,42 @@ main ( int argc, char *argv[] )
       
 	size_t global_work1[3]  = {BLOCK_SIZE, 1, 1};
 	size_t local_work1[3]  = {BLOCK_SIZE, 1, 1};
-	err = clEnqueueNDRangeKernel(cmd_queue, diagnal, 2, NULL, global_work1, local_work1, 0, 0, 0);
+	err = clEnqueueNDRangeKernel(cmd_queue, diagnal, 2, NULL, global_work1, local_work1, 0, 0, &event);
 	if(err != CL_SUCCESS) { printf("ERROR:  diagnal clEnqueueNDRangeKernel()=>%d failed\n", err); return -1; }	
-	clFinish(cmd_queue);
-	
-	err = clEnqueueReadBuffer(cmd_queue, d_m, 1, 0, matrix_dim*matrix_dim*sizeof(float), m, 0, 0, 0);
-	if(err != CL_SUCCESS) { printf("ERROR: clEnqueueReadBuffer  d_m (size:%lu) => %d\n", matrix_dim*matrix_dim, err); return -1; }
-	clFinish(cmd_queue);
-	/* end of timing point */
-	stopwatch_stop(&sw);
-	printf("Time consumed(ms): %lf\n", 1000*get_interval_by_sec(&sw));
+#ifdef TIMING
+    kernel_time += probe_event_time(event,cmd_queue);
+#endif
+    clReleaseEvent(event);
 
+	clFinish(cmd_queue);
+	err = clEnqueueReadBuffer(cmd_queue, d_m, 1, 0, matrix_dim*matrix_dim*sizeof(float), m, 0, 0, &event);
+	if(err != CL_SUCCESS) { printf("ERROR: clEnqueueReadBuffer  d_m (size:%lu) => %d\n", matrix_dim*matrix_dim, err); return -1; }
+#ifdef TIMING
+    d2h_time += probe_event_time(event,cmd_queue);
+#endif
+    clReleaseEvent(event);
+
+	clFinish(cmd_queue);
+
+#ifdef  TIMING
+    gettimeofday(&tv_close_start, NULL);
+#endif
 	clReleaseMemObject(d_m);
+#ifdef  TIMING
+	gettimeofday(&tv_close_end, NULL);
+	tvsub(&tv_close_end, &tv_close_start, &tv);
+	close_time = tv.tv_sec * 1000.0 + (float) tv.tv_usec / 1000.0;
+	tvsub(&tv_close_end, &tv_total_start, &tv);
+	total_time = tv.tv_sec * 1000.0 + (float) tv.tv_usec / 1000.0;
+
+	printf("Init: %f\n", init_time);
+	printf("MemAlloc: %f\n", mem_alloc_time);
+	printf("HtoD: %f\n", h2d_time);
+	printf("Exec: %f\n", kernel_time);
+	printf("DtoH: %f\n", d2h_time);
+	printf("Close: %f\n", close_time);
+	printf("Total: %f\n", total_time);
+#endif
 
 	if (do_verify){
 		printf("After LUD\n");
