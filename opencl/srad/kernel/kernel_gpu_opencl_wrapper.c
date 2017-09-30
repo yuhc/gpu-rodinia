a64ec0c0557b2a7524e63ff33ff1ab8532060851
Hangchen Yu <hyu@cs.utexas.edu>
2017-09-30 14:03:24 -0500

    [ocl/srad] hide progress output, and fix Makefile

--- opencl/srad/kernel/kernel_gpu_opencl_wrapper.c
+++ opencl/srad/kernel/kernel_gpu_opencl_wrapper.c
@@ -891,12 +891,12 @@ kernel_gpu_opencl_wrapper(	fp* image,											// input image
 	// 	COMPUTATION
 	//======================================================================================================================================================150
 
-	printf("Iterations Progress: ");
+	printf("Iterations Progress: hidden");
 
 	// execute main loop
 	for (iter=0; iter<niter; iter++){										// do for the number of iterations input parameter
 
-		printf("%d ", iter);
+		// printf("%d ", iter);
 		fflush(NULL);
 
 		//====================================================================================================100
